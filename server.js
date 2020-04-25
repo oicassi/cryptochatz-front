643163ff938878a3ee42a5f56631aa415905ac2c
Cassiano Kruchelski Vidal <casst@protonmail.com>
2020-04-25 15:26:05 -0300

    path correction

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const path = require('path');
+// const path = require('path');
 
 // Run app serving static files in dist directory
 
@@ -8,8 +8,8 @@ app.use(express.static(__dirname + '/dist'));
 
 /// For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
-});
+// app.get('/*', function(req, res) {
+//   res.sendFile(path.join(__dirname + '/dist/index.html'));
+// });
 
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 4200);
