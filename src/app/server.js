8f5de14d74246c60b5f91ff2f4005c51a612678c
Cassiano Kruchelski Vidal <casst@protonmail.com>
2020-04-25 14:57:56 -0300

    add server files for deploy

---
+++ src/app/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+// Run app serving static files in dist directory
+
+app.use(express.static(__dirname + '/dist'));
+
+/// For all GET requests, send back index.html
+// so that PathLocationStrategy can be used
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
+
+app.listen(process.env.PORT || 8080);
