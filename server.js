5a7e518222758bbcc920c14ac77d1d94fa1ca447
Cassiano Kruchelski Vidal <casst@protonmail.com>
2020-04-25 15:41:21 -0300

    cadd changes to deploy heroku

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 // const path = require('path');
 
 // Run app serving static files in dist directory
-
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/CryptoChat'));
 
 /// For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
